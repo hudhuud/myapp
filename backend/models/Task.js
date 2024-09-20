58093e75553e0f5b9569fec40afa65419b108cca
hudhuud <118181770+hudhuud@users.noreply.github.com>
2024-09-20 15:51:56 +0300

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,16 @@
-///
+const mongoose = require('mongoose');
+
+// Схема задачи
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// Экспорт модели
+module.exports = mongoose.model('Task', TaskSchema);
